997faf0cadea47ba07518849b053933433da6d55
Shine Kim <shinexkim@gmail.com>
2020-06-19 20:27:22 -0700

    Added WebApp Build from React

---
+++ src/main/webapp/precache-manifest.070f7b426927293e26eb7a83befc7ee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ba767cb794c03c680a262ec330e7c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b388ce1c3d66b20e4dae",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e27719a4809d63c5ff3e",
+    "url": "/static/css/main.80717ba7.chunk.css"
+  },
+  {
+    "revision": "b388ce1c3d66b20e4dae",
+    "url": "/static/js/2.fd6c0e76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fd6c0e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27719a4809d63c5ff3e",
+    "url": "/static/js/main.13ecb0bc.chunk.js"
+  },
+  {
+    "revision": "aa76ee2d9e647a552a38",
+    "url": "/static/js/runtime-main.8f498959.js"
+  }
+]);
--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ed357eec0a527ac3e6689e9ee2239e5.js"
+  "/precache-manifest.070f7b426927293e26eb7a83befc7ee8.js"
 );
 
 self.addEventListener('message', (event) => {
